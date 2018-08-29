ba12d3e4157b7c2dc42b83dbcddb02ab5a5d4d7e
Sebastijan Kaplar <sebastijan.kaplar@gmail.com>
2018-08-29 12:42:31 +0200

    Added initial login screen.

---
+++ eUsluge/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  eUsluge
+//
+//  Created by Sebastijan Kaplar on 8/29/18.
+//  Copyright © 2018 Sebastijan Kaplar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        if emailTextField.text == "pera@pera.com" && passwordTextField.text == "pera" {
+         performSegue(withIdentifier: "homeSegue", sender: self)
+        } else {
+            let alert = UIAlertController(title: "Not found!", message: "Incorrect credentials!", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
